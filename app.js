4b05736d8252f3522a9fcee2ad46597772fb9ce5
elbek <elbeksaparbayev05@gmail.com>
2025-10-25 16:03:25 +0500

    operatrlar

---
+++ app.js
@@ -0,0 +1,47 @@
+console.log("taqqoslash operatrlari")
+// taqqoslash operatrlari
+console.log(Boolean(10==10))
+console.log(Boolean(10=="10"))
+console.log(Boolean(10==="10"))
+
+console.log(Boolean(10!="10"))
+console.log(Boolean(10!=="10"))
+
+console.log(Boolean(10<10))
+console.log(Boolean(10>9))
+
+console.log(Boolean(10<=10))
+console.log(Boolean(10>=10))
+// mantiqiy operatrlari
+console.log("mantiqiy operatrlari")
+console.log(Boolean(10<=10 && 5>9))
+console.log(Boolean(10<=10 || 5>9))
+
+console.log(Boolean(!(10<=10 && 5>9)))
+console.log(Boolean(!(10<=10 || 5>9)))
+// belgilash operatrlari
+console.log("belgilash operatrlari")
+let x = 5
+console.log(x)
+
+x += 5
+console.log(x)
+
+
+let y = 5
+console.log(y)
+
+y -= 5
+console.log(y)
+
+let z = 5
+console.log(z)
+
+z /= 5
+console.log(z)
+
+let a = 5
+console.log(a)
+
+a *= 5
+console.log(a)
